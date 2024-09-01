f27b91812dbf950726a4449eac2932448546dd5f
AnikethDeb <techrunstheworld@gmail.com>
2024-09-01 10:28:25 +0530

    feat:ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import Landing from './Components/Landing';
-import Navbar from './Components/Navbar';
+// import React from 'react'
+// import Landing from './Components/Landing';
+// import Navbar from './Components/Navbar';
+// function App() {
+//   return (
+//     <>
+//    <div className='w-full min-h-screen bg-white text-black '>
+//     <Navbar></Navbar>
+//     <Landing></Landing>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
+import newPage from './Components/pages/newPage'// Adjust the import path accordingly
+
 function App() {
   return (
-    <>
-   <div className='w-full min-h-screen bg-white text-black '>
-    <Navbar></Navbar>
-    <Landing></Landing>
-    </div>
-    </>
-  )
+    <Router>
+      <div className='w-full min-h-screen bg-white text-black'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/new-page" element={<newPage />} /> {/* Define the route for the new page */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import img from '../assets/circle-fill.svg';
+
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/new-page'); 
+  };
+
   return (
-    <div className='parernt-navbar flex justify-around flex-row w-[98%] h-[10vh] fixed border-red-400  z-10 border-2 ml-4  mt-4 '>
-        <div className='logo w-[20%] h-[100%] border-2 '></div>
+    <div className='parernt-navbar flex justify-around flex-row w-[98%] h-[10vh] fixed border-red-400 z-10 border-2 ml-4 mt-4'>
+      <div className='logo w-[20%] h-[100%] border-2'></div>
 
-        <div className='middle flex items-end justify-start border-2 w-[55%]'>
-            {["About Us" ,"Procedures" , "Skincare" , "Reviews"].map(
-                (items,index)=>( <a href="" className='font-light text-md text-md mr-6 hover:text-blue-400 font-[mona] '>{items}</a> )
-            )}
-        </div>
+      <div className='middle flex items-end justify-start border-2 w-[55%]'>
+        {["About Us", "Procedures", "Skincare", "Reviews"].map((item, index) => (
+          <a href="" key={index} className='font-light text-md mr-6 hover:text-blue-400 font-[mona]'>
+            {item}
+          </a>
+        ))}
+      </div>
 
-        <div className='right w-[20%] h-[100%] flex items-center  border-2 border-green-200'>
-            <div className='button w-[50%] h-[60%] bg-[#FCFCFC] text-black border-2 rounded-full text-sm font-[600] flex items-center border-red-300 px-[0.6vw]'>Start program
-            </div>
+      <div className='right w-[20%] h-[100%] flex items-center border-2 border-green-200'>
+        <div
+          className='button w-[50%] h-[60%] bg-[#FCFCFC] text-black border-2 rounded-full text-sm font-[600] flex items-center border-red-300 px-[0.6vw]'
+          onClick={handleButtonClick} // Add the click handler
+        >
+          Start program
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
